2df41a3d1ca67fcdcb5c718347eeada64787bf14
jiri.strouhal <jiri.strouhal@gmail.com>
2024-01-10 09:45:16 +0100

    Renam input_openapi.yaml to openapi.yaml and move it to root folder
    openapi.yaml is recommended name, see https://swagger.io/specification/

--- regen.sh
+++ regen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd server
-npx @openapitools/openapi-generator-cli generate -i ../input_openapi_yaml/input_openapi.yaml \
+npx @openapitools/openapi-generator-cli generate -i ../openapi.yaml \
 -g python-flask -o . --additional-properties=packageName=fleetman_http_api
 popd
