a83181bec91e636413d9369415a31d48f29249ad
jiri.strouhal <jiri.strouhal@gmail.com>
2024-01-17 14:44:46 +0100

    Test repeatedly sending car state returns code 400

--- tests/controllers/test_car_state_controller.py
+++ tests/controllers/test_car_state_controller.py
@@ -9,31 +9,38 @@ class Test_Adding_State_Of_Existing_Car(unittest.TestCase):
 
     def setUp(self) -> None:
         connection.set_test_connection_source()
+        self.car = Car(id=1, name="Test Car", platform_id=5)
+        self.app = get_app().app
+        with self.app.test_client() as c:
+            c.post('/v1/car', json=self.car.to_dict())
 
     def test_adding_state_to_existing_car(self):
-        car = Car(id=1, name="Test Car", platform_id=5)
         gnss_position = GNSSPosition(latitude=48.8606111, longitude=2.337644, altitude=50)
         car_state = CarState(id=12, status="idle", car_id=1, speed=7, fuel=80, position=gnss_position)
-        app = get_app()
-        with app.app.test_client() as c:
-            c.post('/v1/car', json=car.to_dict())
+        with self.app.test_client() as c:
             response = c.post('/v1/carstate', json=car_state.to_dict())
             self.assertEqual(response.status_code, 200)
 
     def test_adding_state_to_nonexisting_car_returns_code_404(self):
+        nonexistent_car_id = 121651516
         gnss_position = GNSSPosition(latitude=48.8606111, longitude=2.337644, altitude=50)
-        car_state = CarState(id=12, status="idle", car_id=1, speed=7, fuel=80, position=gnss_position)
-        app = get_app()
-        with app.app.test_client() as c:
+        car_state = CarState(id=12, status="idle", car_id=nonexistent_car_id, speed=7, fuel=80, position=gnss_position)
+        with self.app.test_client() as c:
             response = c.post('/v1/carstate', json=car_state.to_dict())
             self.assertEqual(response.status_code, 404)
 
     def test_sending_incomplete_state_returns_code_400(self):
-        car = Car(id=1, name="Test Car", platform_id=5)
-        app = get_app()
-        with app.app.test_client() as c:
-            c.post('/v1/car', json=car.to_dict())
+        with self.app.test_client() as c:
             response = c.post('/v1/carstate', json={})
+            self.assertEqual(response.status_code, 400)
+
+    def test_sending_repeatedly_status_with_identical_id_returns_code_400(self):
+        gnss_position = GNSSPosition(latitude=48.8606111, longitude=2.337644, altitude=50)
+        car_state = CarState(id=12, status="idle", car_id=1, speed=7, fuel=80, position=gnss_position)
+        with self.app.test_client() as c:
+            response = c.post('/v1/carstate', json=car_state.to_dict())
+            self.assertEqual(response.status_code, 200)
+            response = c.post('/v1/carstate', json=car_state.to_dict())
             self.assertEqual(response.status_code, 400)
 
 
