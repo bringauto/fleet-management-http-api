56b10484b750879584a5e24e7134e9cc65ef57d3
jiri.strouhal <jiri.strouhal@gmail.com>
2024-01-31 14:57:49 +0100

    Add method for composing uri for mutliple parts and prevent multiple slashes in the result

--- fleet_management_api/api_impl/security.py
+++ fleet_management_api/api_impl/security.py
@@ -1,5 +1,7 @@
-from keycloak import KeycloakOpenID
-from urllib.parse import urlparse
+import urllib.parse as _url
+
+from keycloak import KeycloakOpenID # type: ignore
+
 
 class SecurityObj:
     def set_config(self, keycloak_url: str, client_id: str, secret_key: str, scope: str, realm: str, base_uri: str) -> None:
@@ -7,7 +9,7 @@ class SecurityObj:
         self._keycloak_url = keycloak_url
         self._scope = scope
         self._realm_name = realm
-        self._callback = base_uri + "/token_get"
+        self._callback = appended_uri(base_uri, "token_get")
         self._state = "state"
 
         self._oid = KeycloakOpenID(
@@ -30,8 +32,8 @@ class SecurityObj:
         """Get token from keycloak using a code returned by keycloak."""
         if state != self._state:
             raise Exception("Invalid state")
-        
-        if urlparse(iss).geturl() != self._keycloak_url + "/realms/" + self._realm_name:
+
+        if _url.urlparse(iss).geturl() != appended_uri(self._keycloak_url, "realms", self._realm_name):
             raise Exception("Invalid issuer")
 
         token = self._oid.token(
@@ -40,10 +42,24 @@ class SecurityObj:
             redirect_uri=self._callback
         )
         return token
-    
+
     def token_refresh(self, refresh_token: str) -> dict:
         """Get a new token from keycloak using the refresh token."""
         token = self._oid.refresh_token(
             refresh_token=refresh_token
         )
         return token
+
+
+def appended_uri(uri: str, *appended: str) -> str:
+    """Join URI parts.
+
+    This function return valid URI composed of multiple parts.
+    """
+    if uri.endswith("//"):
+        raise ValueError("Invalid URI: " + uri)
+    if appended and not uri.endswith("/"):
+        uri += "/"
+    for part in appended:
+        uri = _url.urljoin(base=uri+"/", url=part.strip("/"))
+    return uri
