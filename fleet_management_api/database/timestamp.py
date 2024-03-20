9eb85adebbe578e66d7c6e386c345f8dc5499bca
jiri.strouhal <jiri.strouhal@gmail.com>
2024-03-20 08:45:41 +0100

    Fix timestamp conversion from s to ms
    The previous code version did erroneously produced timestamp in ms, but with a second resolution

--- fleet_management_api/database/timestamp.py
+++ fleet_management_api/database/timestamp.py
@@ -2,8 +2,8 @@ import time
 
 
 def timestamp_ms() -> int:
-    return 1000 * _get_time_in_ms()
+    return _get_time_in_ms()
 
 
 def _get_time_in_ms() -> int:
-    return int(time.time())
+    return int(1000 * time.time())
