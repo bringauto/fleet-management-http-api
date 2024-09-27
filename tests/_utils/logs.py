9325d8dda62c7040a4c1968f92edad3228f0d807
jiri.strouhal <jiri.strouhal@gmail.com>
2024-09-27 11:36:17 +0200

    Add detailed error handling in clear_logs

--- tests/_utils/logs.py
+++ tests/_utils/logs.py
@@ -4,10 +4,17 @@ import os
 
 def clear_logs() -> None:
     """Find and clear the log file."""
-    dir = os.path.dirname(__file__)
-    config = json.loads(open(os.path.join(dir,"test_config.json")).read())
-    log_file_path = config["logging"]["log-path"]
-    for file in os.listdir(log_file_path):
-        if os.path.isfile(file) and file.endswith(".log"):
-            with open(file, "w") as f:
-                f.write("")
+    try:
+        dir = os.path.dirname(__file__)
+        config = json.loads(open(os.path.join(dir,"test_config.json")).read())
+        log_file_path = config["logging"]["log-path"]
+        for file in os.listdir(log_file_path):
+            if os.path.isfile(file) and file.endswith(".log"):
+                with open(file, "w") as f:
+                    f.write("")
+    except KeyError as e:
+        print("Missing key in configuration file: ", e)
+    except FileNotFoundError as e:
+        print("Configuration file not found: ", e)
+    except Exception as e:
+        print("Unexpected error when clearing up log files: ", e)
