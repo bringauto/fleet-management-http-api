4596870c946faa583bb850ef361d17e00eadb8aa
jiri.strouhal <jiri.strouhal@gmail.com>
2025-01-31 14:30:56 +0100

    Add test helper method waiting for api instance process to start

--- tests/client/test_client_authorization_and_tenant.py
+++ tests/client/test_client_authorization_and_tenant.py
@@ -1,6 +1,6 @@
 import multiprocessing
+import time
 
-from fleet_management_api.models import PlatformHW
 import fleet_management_api.app as _app
 from fleet_management_api.api_impl.auth_controller import (
     generate_test_keys,
@@ -9,16 +9,43 @@ from fleet_management_api.api_impl.auth_controller import (
     clear_test_keys,
     get_test_public_key,
 )
-from fleet_management_api.app import get_token
+from fleet_management_api.app import get_token, TestApp
+from fleet_management_http_client_python import (
+    ApiClient,
+    Configuration,
+    PlatformHWApi,
+    TenantApi,
+    ApiApi,
+)
+from fleet_management_http_client_python.exceptions import UnauthorizedException
+from fleet_management_http_client_python.models import PlatformHW as PlatformHWFor
 
 import tests._utils.api_test as api_test
-from fleet_management_http_client_python import ApiClient, Configuration, PlatformHWApi, TenantApi
-from fleet_management_http_client_python.exceptions import UnauthorizedException
-import time
 
 
 def stop_app():
     raise RuntimeError("Stopping the app")
+
+
+def wait_for_process(app: TestApp, process: multiprocessing.Process, timeout: float = 10):
+    process.start()
+    retry_interval = 1
+    start = time.time()
+    host = "http://localhost:8080/v2/management"
+    client = ApiClient(
+        configuration=Configuration(host=host, api_key={"APIKeyAuth": "APIKey"}),
+        cookie="tenant=test-tenant",
+    )
+    api_api = ApiApi(client)
+    while time.time() - start < timeout:
+        try:
+            api_api.check_api_is_alive()
+            break
+        except:
+            time.sleep(retry_interval)
+    else:
+        process.terminate()
+        raise RuntimeError("Failed to start test server")
 
 
 class Test_(api_test.TestCase):
@@ -33,8 +60,7 @@ class Test_(api_test.TestCase):
         self.p = multiprocessing.Process(
             target=self.app.run, kwargs={"port": 8080, "debug": False, "use_reloader": False}
         )
-        self.p.start()
-        time.sleep(1)
+        wait_for_process(self.app, self.p)
 
     def test_using_correct_api_key_and_specifying_tenant_cookie_allows_to_post_and_read_data(
         self,
@@ -47,7 +73,7 @@ class Test_(api_test.TestCase):
             cookie="tenant=test-tenant",
         )
         platform_hw_api = PlatformHWApi(client)
-        platform_hw_api.create_hws(platform_hw=[PlatformHW(name="test_hw").to_dict()])
+        platform_hw_api.create_hws(platform_hw=[PlatformHWFor(name="test_hw")])
         tenant_api = TenantApi(client)
         self.assertEqual([hw.name for hw in platform_hw_api.get_hws()], ["test_hw"])
         self.assertEqual([t.name for t in tenant_api.get_tenants()], ["test-tenant"])
@@ -63,7 +89,7 @@ class Test_(api_test.TestCase):
             cookie="tenant=test-tenant",
         )
         platform_hw_api = PlatformHWApi(client)
-        response = platform_hw_api.create_hws(platform_hw=[PlatformHW(name="test_hw").to_dict()])
+        response = platform_hw_api.create_hws(platform_hw=[PlatformHWFor(name="test_hw")])
         self.assertEqual([hw.name for hw in response], ["test_hw"])
 
     def test_posting_data_without_any_authorization_raises_exception(
@@ -75,7 +101,7 @@ class Test_(api_test.TestCase):
         )
         platform_hw_api = PlatformHWApi(client)
         with self.assertRaises(UnauthorizedException):
-            platform_hw_api.create_hws(platform_hw=[PlatformHW(name="test").to_dict()])
+            platform_hw_api.create_hws(platform_hw=[PlatformHWFor(name="test").to_dict()])
 
     def tearDown(self) -> None:
         self.p.terminate()
