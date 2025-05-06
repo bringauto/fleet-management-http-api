e0c41d4125cf4d1016a0184269c6dbf9ecf6a61d
jiri.strouhal <jiri.strouhal@gmail.com>
2025-05-06 13:32:39 +0200

    Add docstrings to the controller decorators

--- fleet_management_api/api_impl/controller_decorators.py
+++ fleet_management_api/api_impl/controller_decorators.py
@@ -21,6 +21,12 @@ P = ParamSpec("P")
 def controller_with_tenants(
     controller: Callable[Concatenate[_AccessibleTenants, P], _Response],
 ) -> Callable[Concatenate[P], _Response]:
+    """This decorator provides the controller function with the information about tenants, for which data can be read or modified.
+
+    The controller also prevents calling the controller if
+    1) the request body is in invalid format or
+    2) tenants are not set up correctly, either the tenant cookie is not set for modifying the data or there are no accessible tenants
+    """
 
     def wrapper(*args, **kwargs):
         request = _RequestEmpty.load()
@@ -40,6 +46,13 @@ def controller_with_tenants(
 def controller_with_tenants_and_data(
     controller: Callable[Concatenate[_AccessibleTenants, list[dict], P], _Response],
 ) -> Callable[Concatenate[P], _Response]:
+    """This decorator provides the controller function with the information about tenants, for which data can be created or modified
+    and the data itself (in JSON format).
+
+    The controller also prevents calling the controller if
+    1) the request body is not a valid JSON or
+    2) tenants are not set up correctly, either the tenant cookie is not set for modifying the data or there are no accessible tenants
+    """
 
     def wrapper(*args, **kwargs):
         request = _RequestJSON.load()
