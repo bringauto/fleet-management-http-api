1dfb757d78c77a27a5da2ee8ce1b8dda3815682d
jiri.strouhal <jiri.strouhal@gmail.com>
2025-05-09 12:56:06 +0200

    Fix tenant extraction from JWT token
    THe previous version expected the extracted payload to contain item 'payload' - this is not the case, that decoded object contains directly the 'group' item.

--- fleet_management_api/api_impl/constants.py
+++ fleet_management_api/api_impl/constants.py
@@ -1,3 +1,7 @@
+"""Constants used in the Fleet Management API implementation, namely by the tenant-related modules."""
+
 AUTHORIZATION_HEADER_NAME = "Authorization"
 AUTHORIZATION_ENVIRONMENT_NAME = "HTTP_AUTHORIZATION"
-PAYLOAD_FIELD_NAME = "payload"
+TENANT_PAYLOAD_ITEM = (
+    "group"  # The name of the field in the JWT payload that contains the tenant information.
+)
